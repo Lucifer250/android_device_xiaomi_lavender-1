10e348d6d1e393775b872e31a8df4eb7a7ea5ea4
Pika Pika <52284253+PikaPikaFreed@users.noreply.github.com>
2019-07-11 20:31:30 +0800

    Initial : COSP

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo candy_lavender-userdebug
+add_lunch_combo cosp_lavender-userdebug
